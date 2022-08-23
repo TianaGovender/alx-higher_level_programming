b8f1354d94d59728c8f03b9d6964ae62cd817ca9
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-08-23 20:51:53 +0200

    Edited indents

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
-	for j in range(10):
-		if i < j:
-			print("{:d}{:d},".format(i, j), end=" ")
+    for j in range(10):
+	if i < j:
+           print("{:d}{:d},".format(i, j), end=" ")
