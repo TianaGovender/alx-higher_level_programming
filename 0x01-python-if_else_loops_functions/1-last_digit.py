3b68b74190b549a51c45122291923574f107fd1c
TianaGovender <tianagovender1@gmail.com>
2022-08-23 11:29:24 +0000

    Task 1 recheck

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
 n_str = repr(number)
 last = int(n_str[-1])
+if number < 0:
+	last = -1 * last
 if last > 5:
 	print("Last digit of {:d} is {:d} and is greater than 5".format(number, last))
 if last < 6 and last != 0:
