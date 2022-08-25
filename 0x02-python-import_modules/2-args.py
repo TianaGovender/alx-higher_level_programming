1fc3b8e31d338e698e0829af98128d000d71ef5c
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-08-25 16:01:06 +0200

    edited indents

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   """Print the number of and list the arguments"""
-   import sys
+    """Print the number of and list the arguments"""
+    import sys
 
-   len = (len(sys.argv) - 1)
-   if len == 0:
-      print("0 arguments.")
-   if len == 1:
-      print("{} argument:".format(len))
-   if len > 1:
-      print("{} arguments:".format(len))
+    len = (len(sys.argv) - 1)
+    if len == 0:
+        print("0 arguments.")
+    if len == 1:
+        print("{} argument:".format(len))
+    if len > 1:
+        print("{} arguments:".format(len))
 
-   i = 1
-   while i <= len:
-      print("{}: {}".format(i, sys.argv[i]))
-      i = i + 1;
+    i = 1
+    while i <= len:
+        print("{}: {}".format(i, sys.argv[i]))
+        i = i + 1;
