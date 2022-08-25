074e151d356c13b7b4ffd2f05a19c47d31d1cad3
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-08-25 16:19:33 +0200

    same code different fiile

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the number of and list the arguments"""
+    """Print the number of and list of arguments."""
     import sys
 
-    len = (len(sys.argv) - 1)
-    if len == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    if len == 1:
-        print("{} argument:".format(len))
-    if len > 1:
-        print("{} arguments:".format(len))
-    i = 1
-    while i <= len:
-        print("{}: {}".format(i, sys.argv[i]))
-        i = i + 1;
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
