bb0d95f764e62cf2ace09eb258b1a6fd187de6c3
Tiana Govender <104756284+TianaGovender@users.noreply.github.com>
2022-08-31 21:46:42 +0200

    Delete 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-
-    for i in matrix:
-        new_matrix.append(list(map(lambda j: j**2, i)))
-
-    return new_matrix    
