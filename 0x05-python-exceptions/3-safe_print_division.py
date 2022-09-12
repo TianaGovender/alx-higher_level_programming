9c14716d57cfae4b6322d7fd92c993fd601310c9
TianaGovender <tianagovender1@gmail.com>
2022-09-12 17:48:06 +0000

    Task 1

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+def safe_print_division(value):
     j = 0
-    for i in range(0,x):
+    for i in (0,x):
         try:
             print("".format(my_list[i]), end="")
             j += 1
         except (ValueError, TypeError):
             continue
-    print("")
     return (j)
