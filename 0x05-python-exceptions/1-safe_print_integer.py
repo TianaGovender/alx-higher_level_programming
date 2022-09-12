c378a1ffa328990da4ebcf5b153afd50c8288498
TianaGovender <tianagovender1@gmail.com>
2022-09-12 15:37:12 +0000

    Task 1and task 2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,23 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-<<<<<<< HEAD
-    i = 0
-    while i <= x:
-        try:
-            print("{}".format(my_list[i]), end="")
-            i += 1
-        except IndexError:
-            break
-    print("")
-    return (i)
-=======
-    total = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
-            total += 1
-        except IndexError:
-            break
-    print("")
-    return (total)
->>>>>>> 0e05c559114fa0672199d2355b5dd2ac2fa23258
+def safe_print_integer(value)
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
