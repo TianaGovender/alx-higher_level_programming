39ff66af0695b0b058e05da0da4d707beb088077
TianaGovender <tianagovender1@gmail.com>
2022-09-12 18:31:54 +0000

    Task 2 task 3 and task 4

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+def safe_print_list_integers(my_list=[], x=0):
     j = 0
-    for i in range(0,x):
+    for i in range(0, x):
         try:
-            print("".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             j += 1
         except (ValueError, TypeError):
             continue
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def safe_print_division(value):
-    j = 0
-    for i in (0,x):
-        try:
-            print("".format(my_list[i]), end="")
-            j += 1
-        except (ValueError, TypeError):
-            continue
-    return (j)
+def safe_print_division(a, b):
+    try:
+        res = a / b
+    except (TypeError, ZeroDivisionError):
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+
+    return (res)
