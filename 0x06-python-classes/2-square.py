93edfaefe62c56213151e08a6d2fc6e32e6df48d
TianaGovender <tianagovender1@gmail.com>
2022-09-13 17:46:58 +0000

    Edited spaces

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,11 +7,11 @@ Does value and type checks
 
 
 class Square:
-        """Initialising size and giving error eceptions
-        """
-	def __init__(self, size=0):
-            if type(size) != int:
-                raise TypeError('size must be an integer')
-            if size < 0:
-                raise ValueError('size must be >= 0')
-            self.__size = size
+    """Initialising size and giving error eceptions
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,14 +7,14 @@ Its implements value and type checks for its attributes
 
 
 class Square:
-        """Initialise Square
-        """
-	def __init__(self, size=0):
-            if type(size) != int:
-                raise TypeError('size must be an integer')
-            if size < 0:
-                raise ValueError('size must be >= 0')
-            self.__size = size
+    """Initialise Square
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
 
-        def area(self):
-            return (self.size * self.size)
+    def area(self):
+        return (self.size * self.size)
